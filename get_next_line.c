2523a77f6fb859ea2a57ea9e2a3f3bfcd4712c76
FK <filip.karika1@gmail.com>
2024-10-21 21:38:08 +0200

    checking SEGFAULT with asset

--- get_next_line.c
+++ get_next_line.c
@@ -31,45 +31,49 @@ char	*read_line(char *str)
 	return (line);
 }
 
-char	*update_static_buffer(char *str)
+int	update_static_buffer(char **str)
 {
 	int		i;
 	char	*new_str;
 	size_t	str_len;
 
 	i = 0;
-	while (str[i] && str[i] != '\n')
+	while ((*str)[i] && (*str)[i] != '\n')
 		i++;
-	if (str[i] == '\n')
+	if ((*str)[i] == '\n')
 		i++;
-	str_len = ft_strlen(str + i);
+	str_len = ft_strlen(*str + i);
 	if (str_len == 0)
 	{
-		free(str);
-		return (NULL);
+		free(*str);
+		*str = NULL;
+		return (0);
 	}
 	new_str = (char *)malloc((str_len + 1) * sizeof(char));
 	if (!new_str)
 	{
-		free(str);
-		return (NULL);
+		free(*str);
+		*str = NULL;
+		return (-1);
 	}
-	ft_strlcpy(new_str, str + i, str_len + 1);
-	free(str);
-	return (new_str);
+	ft_strlcpy(new_str, *str + i, str_len + 1);
+	free(*str);
+	*str = new_str;
+	return (0);
 }
 
 char	*get_next_line(int fd)
 {
 	int				fd_read;
 	char			*tmp;
-	static char		*str_start;
+	static char		*str_start = NULL;
+	char			*tmp_dalsi;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	tmp = (char *)ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 	if (!tmp)
-		return (NULL);
+		return (free(tmp), free(str_start), str_start = NULL, NULL);
 	fd_read = 42;
 	while (!ft_strchr(tmp, '\n') && fd_read != 0)
 	{
@@ -77,10 +81,15 @@ char	*get_next_line(int fd)
 		if (fd_read < 0)
 			return (free(tmp), free(str_start), str_start = NULL, NULL);
 		tmp[fd_read] = '\0';
-		str_start = ft_strjoin(str_start, tmp);
+		tmp_dalsi = ft_strjoin(str_start, tmp);
+		if (tmp_dalsi == NULL)
+			return (free(tmp), free(str_start), str_start = NULL, NULL);
+		free(str_start);
+		str_start = tmp_dalsi;
 	}
 	free(tmp);
 	tmp = read_line(str_start);
-	str_start = update_static_buffer(str_start);
+	if (tmp == NULL || update_static_buffer(&str_start) == -1)
+		return (free(tmp), free(str_start), str_start = NULL, NULL);
 	return (tmp);
 }
